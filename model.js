5f3242df85867b6a016da453b05d7ef81bbeff81
Ahmedu <ahmedu@ahmedu-hp.lan>
2024-12-16 11:12:21 +0000

    Master

---
+++ model.js
@@ -0,0 +1,19 @@
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let current = arr[i];
+        
+        let j = i - 1;
+        
+        while (j >= 0 && arr[j] > current) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        
+        arr[j + 1] = current;
+    }
+    return arr;
+}
+
+// Exemple d'utilisation
+let arr = [12, 11, 13, 5, 6];
+console.log("Tableau trié : ", insertionSort(arr));
